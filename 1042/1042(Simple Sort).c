3d237a7fdef64b1e2f38cdee4186d118f71d8fd1
MaksudulArafin <rocksterdowan@gmail.com>
2020-12-22 16:43:22 +0600

    It's hard.

--- 1042/1042(Simple Sort).c
+++ 1042/1042(Simple Sort).c
@@ -1,8 +1,74 @@
 #include<stdio.h>
 
-
-int main()
+ int
+main ()
 {
+
+int ar[3], c, i, j, min, temp;
+
+int ar1[3];
+
+for (i = 0; i < 3; i++)
+
+    {
+
+scanf ("%d", &ar[i]);
 
 }
 
+
+for(i=0; i<3; i++)
+{
+    ar1[i]=ar[i];
+}
+
+for (i = 0; i < 2; i++)
+
+    {
+
+min = i;
+
+
+for (j = i + 1; j < 3; j++)
+
+	{
+
+if (ar[min] > ar[j])
+
+
+min = j;
+
+
+}
+
+if (min != i)
+
+	{
+
+temp = ar[i];
+
+ar[i] = ar[min];
+
+ar[min] = temp;
+
+}
+
+}
+
+
+for (i = 0; i < 3; i++)
+
+    {
+
+printf ("%d\n", ar[i]);
+
+}
+printf ("\n");
+
+
+for(i=0; i<3; i++)
+{
+    printf ("%d\n", ar1[i]);
+}
+    return 0;
+}
